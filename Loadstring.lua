84f71bca9c7d2305e41bc06f034e2b540a32fad6
Nau <99986409+naufalafif080419@users.noreply.github.com>
2022-10-29 14:02:11 +0800

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/naufalafif080419/NauHub-KeySystem/main/KeySystem.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/baksff/NauHub/master/KeySystem.lua')))()
