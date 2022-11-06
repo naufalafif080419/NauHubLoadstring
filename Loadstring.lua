01f4fd7771236da417a20023cc233730b3a15f15
Nau <99986409+naufalafif080419@users.noreply.github.com>
2022-11-06 19:37:50 +0800

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,6 +1,1 @@
-local Message = Instance.new("Message")
-Message.Parent = game.CoreGui
-Message.Text = "NauHub Loader is undergoing maintenance Paste It At Your Browser. Check https://discord.com/invite/XHrQ6DPzcb "
-setclipboard("https://discord.com/invite/XHrQ6DPzcb")
-wait(10)
-Message:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/baksff/NauHub/master/KeySystem.lua"))()
